49464e811fd9e022f7ee9babc506d51b7d203e8d
twotoque <73447328+twotoque@users.noreply.github.com>
2023-03-19 23:49:31 -0400

    Added barebones framework based on Decay calculator

---
+++ index.js
@@ -0,0 +1,76 @@
+function routeSuggestion (value) {
+    var routeSuggested = document.getElementById("searchContainerID").value;
+    console.log("joe" + routeSuggested);
+}
+
+function routeFinder (value) {
+    // Searches HTML document for ID containing "searchContainerID". Gets it's value
+    var routeSelectedCapitalize = document.getElementById("searchContainerID").value;
+    var routeSelected = routeSelectedCapitalize.toLowerCase();
+
+    // This function basically searches for the value of the routeSelected to the array "routesConst"
+    function routesConstFinder (routesConst) {
+        return routesConst.search === routeSelected;
+    }
+    
+    // Assigns data found in above fucntion to variables
+    let routeName = routesConst.find(routesConstFinder).name;
+    let MFincreaseEM= routesConst.find(routesConstFinder).MFincreaseEM;
+    let MFfreqEM= routesConst.find(routesConstFinder).MFfreqEM;
+    let SincreaseEM= routesConst.find(routesConstFinder).SincreaseEM;
+    let SfreqEM= routesConst.find(routesConstFinder).SfreqEM;
+    let SHincreaseEM= routesConst.find(routesConstFinder).SHincreaseEM;
+    let SHfreqEM= routesConst.find(routesConstFinder).SHfreqEM;
+    let routeNotes= routesConst.find(routesConstFinder).notes;
+    let cuts= routesConst.find(routesConstFinder).cuts;
+
+    // Replaces body text with updated data
+    document.getElementById("routeNameHTML").innerHTML = routeName;
+    document.getElementById("MFfreqEM").innerHTML = "Every " + MFfreqEM;
+    document.getElementById("MFincreaseEM").innerHTML = MFincreaseEM;
+    document.getElementById("SfreqEM").innerHTML = "Every " + SfreqEM;
+    document.getElementById("SincreaseEM").innerHTML = SincreaseEM;
+    document.getElementById("SHfreqEM").innerHTML = "Every " + SHfreqEM;
+    document.getElementById("SHincreaseEM").innerHTML = SHincreaseEM;
+    document.getElementById("notes").innerHTML = routeNotes;
+    
+
+    // Checks if the values are fulfilled to show intended time bubble
+    if (MFfreqEM == null || MFincreaseEM == null) {
+        document.getElementById('earlyMorning').style.display = 'none';
+    } else {
+        document.getElementById('earlyMorning').style.display = '';
+    }
+
+    if (cuts == "no") {
+        document.getElementById('noCuts').style.display = '';
+    } else {
+        document.getElementById('noCuts').style.display = 'none';
+    }
+    
+
+
+}
+
+
+// Function to hide bubble elements if no value is selected
+window.onload = function() {
+    var MFfreqEM, MFincreaseEM, cuts  = null;
+
+    if (MFfreqEM == null || MFincreaseEM == null) {
+        document.getElementById('earlyMorning').style.display = 'none';
+    } else {
+        document.getElementById('earlyMorning').style.display = '';
+    }
+
+    if (cuts == "no") {
+        document.getElementById('noCuts').style.display = '';
+    } else {
+        document.getElementById('noCuts').style.display = 'none';
+    }
+
+
+
+
+
+}
---
+++ routes.js
@@ -0,0 +1,99 @@
+const routesConst = [
+    {
+        search: "36 finch west",
+        searchNum: "36 finch west",
+        name: "36 Finch West",
+        cuts: "yes",
+
+    // ---- MORNING ----
+
+    //Early Morning
+        MFfreqEM: "5 minutes, 30 seconds",
+        MFincreaseEM: "~0 minute, 45 seconds",
+        SfreqEM: "+1 minute, 0 seconds",
+        SincreaseEM: "+1 minute, 0 seconds",
+        SHfreqEM: "+1 minute, 0 seconds",
+        SHincreaseEM: "+1 minute, 0 seconds",
+
+    //Morning Peak
+        MFfreqEP: "5 minutes, 30 seconds",
+        MFincreaseEP: "~0 minute, 45 seconds",
+        SfreqEP: "+1 minute, 0 seconds",
+        SincreaseEP: "+1 minute, 0 seconds",
+        SHfreqEP: "+1 minute, 0 seconds",
+        SHincreaseEP: "+1 minute, 0 seconds",
+    
+    //Late Morning
+        MFfreqLM: "+1 minute, 0 seconds",
+        MFincreaseLM: "+1 minute, 0 seconds",
+        SfreqLM: "+1 minute, 0 seconds",
+        SincreaseLM: "+1 minute, 0 seconds",
+        SHfreqLM: "+1 minute, 0 seconds",
+        SHincreaseLM: "+1 minute, 0 seconds",
+
+    // ---- AFTERNOON ----
+
+    //Early Afternoon
+        MFfreqEA: "5 minutes, 30 seconds",
+        MFincreaseEA: "~0 minute, 45 seconds",
+        SfreqEA: "+1 minute, 0 seconds",
+        SincreaseEA: "+1 minute, 0 seconds",
+        SHfreqEA: "+1 minute, 0 seconds",
+        SHincreaseEA: "+1 minute, 0 seconds",
+
+    // Afternoon Peak
+        MFfreqAP: "+1 minute, 0 seconds",
+        MFincreaseAP: "+1 minute, 0 seconds",
+        SfreqAP: "+1 minute, 0 seconds",
+        SincreaseAP: "+1 minute, 0 seconds",
+        SHfreqAP: "+1 minute, 0 seconds",
+        SHincreaseAP: "+1 minute, 0 seconds",
+
+        
+    // ---- NIGHT ----
+
+    //Early Evening
+        MFfreqEE: "5 minutes, 30 seconds",
+        MFincreaseEE: "~0 minute, 45 seconds",
+        SfreqEE: "+1 minute, 0 seconds",
+        SincreaseEE: "+1 minute, 0 seconds",
+        SHfreqEE: "+1 minute, 0 seconds",
+        SHincreaseEE: "+1 minute, 0 seconds",
+
+    //Late Evening
+        MFfreqLE: "+1 minute, 0 seconds",
+        MFincreaseLE: "+1 minute, 0 seconds",
+        SfreqLE: "+1 minute, 0 seconds",
+        SincreaseLE: "+1 minute, 0 seconds",
+        SHfreqLE: "+1 minute, 0 seconds",
+        SHincreaseLE: "+1 minute, 0 seconds",
+
+    
+    // ---- MISC ----
+
+    //Midday
+        MFfreqMD: "~6 minutes, 15 seconds",
+        MFincreaseMD: "~+0 minutes, 30 seconds",
+        SfreqMD: "+1 minute, 0 seconds",
+        SincreaseMD: "+1 minute, 0 seconds",
+        SHfreqMD: "+1 minute, 0 seconds",
+        SHincreaseMD: "+1 minute, 0 seconds",
+
+        
+    //Overnight
+        MFfreqMD: "~6 minutes, 15 seconds",
+        MFincreaseMD: "~+0 minutes, 30 seconds",
+        SfreqMD: "+1 minute, 0 seconds",
+        SincreaseMD: "+1 minute, 0 seconds",
+        SHfreqMD: "+1 minute, 0 seconds",
+        SHincreaseMD: "+1 minute, 0 seconds",
+
+    //Notes
+        notes: "Some cuts (speicifcally Monday-Friday at morning peak, midday, afternoon peak, early evening, and Sunday/Holiday morning and afternoon are dependent on if the bus is east or west of Finch West station. The specific times are as follows: 5minutes 30 seconds east of Finch West",
+    },
+    {
+        search: "39 finch east",
+        name: "39 Finch East",
+        cuts: "no",
+    }
+];
